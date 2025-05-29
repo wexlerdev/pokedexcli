2e44e263b1cf3a86eb8dedeac57f1c8be7afbb55
wexlerdev <alexwexlerdev@gmail.com>
2025-05-28 21:56:19 -0500

    finished map and bmap v1

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name		string
 	description	string
-	callback	func() error
+	callback	func(*config) error
 }
 
 type config struct {
@@ -25,12 +25,12 @@ type config struct {
 }
 
 
-func commandExit() error {
+func commandExit(_ * config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
-func commandHelp() error {
+func commandHelp(_ * config) error {
 	helpString := `Welcome to the Pokedex!
 Usage:
 help: Displays a help message
@@ -39,17 +39,58 @@ exit: Exit the Pokedex`
 	return nil
 }
 
-func commandMap(config *config) error {
+func mapWithNoPageChange(config *config) error {
 	const pokeBaseApiUrl = "https://pokeapi.co/api/v2/location-area?limit=20"
 	offset := (config.MapPage - 1) * 20
 
 	pokeLocationApiUrl := pokeBaseApiUrl + fmt.Sprintf("&offset=%v", offset)
 
+	locations, err := pokeapi.GetLocationAreas(pokeLocationApiUrl)
+	if err != nil {
+		return err
+	}
 
+	for _, location := range locations {
+		fmt.Println(location)
+	}
+	
+	return nil
 }
+
+func commandMap(config *config) error {
+	//increment page
+	config.MapPage++
+
+	err := mapWithNoPageChange(config)
+	if err != nil {
+		return err
+	}
+
+	
+	return nil
+}
+
+func commandBmap(config *config) error {
+	if config.MapPage == 0 {
+		config.MapPage = 1
+	}
+
+	if config.MapPage > 1 {
+		config.MapPage--
+	}
+
+	err := mapWithNoPageChange(config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	config := config{0}
 
 	commandRegistry := map[string]cliCommand {
 		"exit": {
@@ -62,17 +103,30 @@ func main() {
 			description:	"helps with cli tool",
 			callback: commandHelp,
 		},
+		"map": {
+			name:			"map",
+			description:	"shows the next 20 locations",
+			callback:		commandMap,
+		},
+		"bmap": {
+			name:			"bmap",
+			description:	"shows the last 20 locations",
+			callback:		commandBmap,
+		},
 	}
 
 	for {
+
 		fmt.Print("Pokedex > ")
 		scanner.Scan() 
 		line := scanner.Text()
 		stringSlice := cleanInput(line)
+		
 		if stringSlice == nil {
 			fmt.Println("stringslice nil ")
 			continue
 		}
+
 		if len(stringSlice) == 0 {
 			fmt.Println("stringslice empty ")
 			continue
@@ -83,7 +137,8 @@ func main() {
 			fmt.Println("Unknown command")
 			continue
 		}
-		err := cmd.callback()
+
+		err := cmd.callback(&config)
 		if err != nil {
 			fmt.Printf("ERRORROROROR")
 			os.Exit(1)
--- pokeapi/pokeapi.go
+++ pokeapi/pokeapi.go
@@ -1,7 +1,7 @@
 package pokeapi
 
 import (
-	"http"
+	"net/http"
 	"encoding/json"
 	"fmt"
 )
@@ -16,9 +16,8 @@ type PokeLocationData struct {
 	} `json:"results"`
 }
 
-const pokeBaseApiUrl = "https://pokeapi.co/api/v2/location-area?limit=20&offset=0"
 
-func getLocationAreas(url string)([]string, error) {
+func GetLocationAreas(url string)([]string, error) {
 
 	res, err := http.Get(url)
 	if err != nil {
@@ -29,12 +28,12 @@ func getLocationAreas(url string)([]string, error) {
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(&pokeLocationStruct)
 	if err != nil {
-		return nil, fmt.Errof("Failure Decoding Location Bytes")
+		return nil, fmt.Errorf("Failure Decoding Location Bytes")
 	}
 
 	locations := make([]string, 0,20)
-	for i, resultStruct := range pokeLocationStruct.Results {
-		locations := append(locations,resultsStruct.Name)
+	for _, resultsStruct := range pokeLocationStruct.Results {
+		locations = append(locations,resultsStruct.Name)
 	}
 	return locations, nil
 }
